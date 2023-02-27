d78ad5077a48681bbff995a30952f63432331a25
dinhphu <phuongdinhphu41@gmail.com>
2023-02-27 20:06:34 +0700

    create backend

---
+++ src/main/java/com/cougar/CougarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cougar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CougarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CougarBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cougar/CougarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cougar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CougarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
