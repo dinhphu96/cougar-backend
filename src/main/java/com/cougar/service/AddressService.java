7ad4ae338368c8389d454f1453f53b67ad453214
dinhphu <phuongdinhphu41@gmail.com>
2023-03-04 12:57:38 +0700

    toastsmohoi

--- src/main/java/com/cougar/service/AddressService.java
+++ src/main/java/com/cougar/service/AddressService.java
@@ -1,5 +1,11 @@
 package com.cougar.service;
 
+import java.util.List;
+
+import com.cougar.entity.Address;
+
 public interface AddressService {
+
+	List<Address> getAddressesByUsserId(Integer userId);
 	
 }
--- src/main/java/com/cougar/service/serviceimpl/AddressServiceImpl.java
+++ src/main/java/com/cougar/service/serviceimpl/AddressServiceImpl.java
@@ -1,11 +1,20 @@
 package com.cougar.service.serviceimpl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cougar.DAO.AddressDAO;
+import com.cougar.entity.Address;
 import com.cougar.service.AddressService;
 @Service
 public class AddressServiceImpl implements AddressService{
 	@Autowired AddressDAO addressDAO;
+
+	@Override
+	public List<Address> getAddressesByUsserId(Integer userId) {
+		// TODO Auto-generated method stub
+		return addressDAO.getAddressesByUsserId(userId);
+	}
 }
