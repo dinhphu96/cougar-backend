fa7ac61965ba3df35e804de9dcaf8aef9663914e
PhanVanThao <94691401+kenduk123@users.noreply.github.com>
2023-03-11 17:18:46 +0700

    thaobackend

--- src/main/java/com/cougar/service/OptionService.java
+++ src/main/java/com/cougar/service/OptionService.java
@@ -1,5 +1,10 @@
 package com.cougar.service;
 
+import java.util.List;
+
+import com.cougar.entity.Option;
+
 public interface OptionService {
 
+	List<Option> findAll();
 }
--- src/main/java/com/cougar/service/serviceimpl/OptionServiceImpl.java
+++ src/main/java/com/cougar/service/serviceimpl/OptionServiceImpl.java
@@ -1,12 +1,19 @@
 package com.cougar.service.serviceimpl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cougar.DAO.OptionDAO;
+import com.cougar.entity.Option;
 import com.cougar.service.OptionService;
 
 @Service
 public class OptionServiceImpl implements OptionService{
 	@Autowired OptionDAO optionDAO;
+	@Override
+	public List<Option> findAll() {
+		return optionDAO.findAll();
+	}
 }
