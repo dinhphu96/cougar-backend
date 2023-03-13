3b2f3118abd43303c18ac29599af831742ae0e7b
dinhphu <phuongdinhphu41@gmail.com>
2023-03-13 16:47:41 +0700

    address

--- src/main/java/com/cougar/restController/AddessesRestController.java
+++ src/main/java/com/cougar/restController/AddessesRestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,8 +32,29 @@ public class AddessesRestController {
 		return addressService.create(address);
 	}
 	
+	@PutMapping("/rest/addresses/{id}")
+	public Address update(@PathVariable("id") Integer id, @RequestBody Address address) {
+		return addressService.create(address); //update
+	}
+	
 	@DeleteMapping("/rest/addresses/{id}")
 	public void delete(@PathVariable("id") Integer id) {
 		addressService.delete(id);
+	}
+	
+	@PutMapping("/rest/addresses/setAsDefault")
+	public List<Address> setAsDefault(@RequestBody Address address){
+		List<Address> list = addressService.getAddressesByUsserId(address.getUser().getId());
+		for (Address add : list) {
+			if(add.getIsDefault() == true) {
+				add.setIsDefault(false);
+				addressService.create(add); 
+			}
+			
+		}
+		address.setIsDefault(true);
+		addressService.create(address); 
+		
+		return list;
 	}
 }
