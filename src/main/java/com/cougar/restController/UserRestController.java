aaebee80b1931d49eb793f99db8488c5a4c7aad0
Nguyễn Mạnh <manhnguyendinh1709@gmail.com>
2023-03-25 00:34:43 +0700

    API
    Create ADMIN, Update ADMIN

--- src/main/java/com/cougar/restController/UserRestController.java
+++ src/main/java/com/cougar/restController/UserRestController.java
@@ -1,13 +1,15 @@
 package com.cougar.restController;
 
+import java.io.IOException;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +17,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import com.cougar.entity.RoleRegister;
 import com.cougar.entity.User;
 import com.cougar.entity.UserLogin;
@@ -28,7 +33,7 @@ import com.cougar.service.UserService;
 
 @CrossOrigin("*")
 @RestController
-@RequestMapping("/rest/users")
+@RequestMapping("/api/users")
 public class UserRestController {
 	@Autowired
 	UserService userService;
@@ -54,20 +59,14 @@ public class UserRestController {
 		return userService.findById(id);
 	}
 
-	// Create Admin
-	@PostMapping("")
-	public ResponseEntity<?> createAdmin(@RequestBody RegisterRequest registerUpRequest) {
-		// Kiểm tra email
+	@PostMapping("/create-admin-with-avatar")
+	public ResponseEntity<?> createAdminWithAvatar(@RequestBody RegisterRequest registerUpRequest) {
 		if (userLoginDAO.existsByEmail(registerUpRequest.getEmail())) {
 			return ResponseEntity.badRequest().body(new MessageResponse("Email already exists!"));
 		}
-
-		// Kiểm tra phone
 		if (userLoginDAO.existsByPhone(registerUpRequest.getPhone())) {
 			return ResponseEntity.badRequest().body(new MessageResponse("Phone already exists!"));
 		}
-
-		// Create new admin's account
 		UserLogin userLogin = new UserLogin();
 		userLogin.setEmail(registerUpRequest.getEmail());
 		userLogin.setPassword(pe.encode(registerUpRequest.getPassword()));
@@ -75,48 +74,93 @@ public class UserRestController {
 		userLogin.setRoles(Collections.singletonList(roles));
 		userLogin.setFullname(registerUpRequest.getFullname());
 		userLogin.setPhone(registerUpRequest.getPhone());
-
-		UserLogin temp = userLoginDAO.save(userLogin);
-
-		return ResponseEntity.ok(userService.findById(temp.getId()));
+		UserLogin adminCreated = userLoginDAO.save(userLogin);
+		Map<String, String> config = new HashMap<>();
+		config.put("cloud_name", "dmjh7imwd");
+		config.put("api_key", "778777726581776");
+		config.put("api_secret", "maiZWuW_V9AF9gIfGJ7ZLHpb3z8");
+		Cloudinary cloudinary = new Cloudinary(config);
+		String avatarNameOnCloud = "avatar-user-id-" + adminCreated.getId();
+		Map params = ObjectUtils.asMap("public_id", avatarNameOnCloud, "overwrite", true, "upload_preset", "cougarstrore");
+		try {
+			Object res = cloudinary.uploader().upload(registerUpRequest.getAvatar(), params);
+			String url = ((Map<String, String>) res).get("url").replace("http://res.cloudinary.com/dmjh7imwd/image/upload/", "");
+			User oldUser = userService.findById(adminCreated.getId());
+			oldUser.setAvatar(url);
+			return ResponseEntity.ok(userService.update(oldUser));
+		} catch (IOException exception) {
+			return ResponseEntity.badRequest()
+					.body(new MessageResponse("Có lỗi khi xử lý hình ảnh, hãy thử upload lại sau."));
+		}
 	}
 
-	@PutMapping("")
-	public ResponseEntity<?> update(@RequestBody User user) {
+	@PostMapping("/create-admin")
+	public ResponseEntity<?> createAdmin(@RequestBody RegisterRequest registerUpRequest) {
+		if (userLoginDAO.existsByEmail(registerUpRequest.getEmail())) {
+			return ResponseEntity.badRequest().body(new MessageResponse("Email already exists!"));
+		}
+		if (userLoginDAO.existsByPhone(registerUpRequest.getPhone())) {
+			return ResponseEntity.badRequest().body(new MessageResponse("Phone already exists!"));
+		}
+		UserLogin userLogin = new UserLogin();
+		userLogin.setEmail(registerUpRequest.getEmail());
+		userLogin.setPassword(pe.encode(registerUpRequest.getPassword()));
+		RoleRegister roles = roleRegisterDAO.findByName("ADMIN").get();
+		userLogin.setRoles(Collections.singletonList(roles));
+		userLogin.setFullname(registerUpRequest.getFullname());
+		userLogin.setPhone(registerUpRequest.getPhone());
+		UserLogin temp = userLoginDAO.save(userLogin);
+		return ResponseEntity.ok(userService.findById(temp.getId()));
+	}
+	
+	@PutMapping("/update-admin-with-avatar")
+	public ResponseEntity<?> updateAdminWithAvatar(@RequestBody User user) {
 		User oldUser = userService.findById(user.getId());
-		
-		// Kiểm tra email - Nếu khác email cũ thì mới kiểm tra
 		if (!user.getEmail().equals(oldUser.getEmail())) {
 			if (userLoginDAO.existsByEmail(user.getEmail())) {
 				return ResponseEntity.badRequest().body(new MessageResponse("Email already exists!"));
 			}
 		}
-		
-		// Kiểm tra phone - Nếu khác phone cũ thì mới kiểm tra
 		if (!user.getPhone().equals(oldUser.getPhone())) {
 			if (userLoginDAO.existsByPhone(user.getPhone())) {
 				return ResponseEntity.badRequest().body(new MessageResponse("Phone already exists!"));
 			}
 		}
-		
+		Map<String, String> config = new HashMap<>();
+		config.put("cloud_name", "dmjh7imwd");
+		config.put("api_key", "778777726581776");
+		config.put("api_secret", "maiZWuW_V9AF9gIfGJ7ZLHpb3z8");
+		Cloudinary cloudinary = new Cloudinary(config);
+		String avatarNameOnCloud = "avatar-user-id-" + user.getId();
+		Map params = ObjectUtils.asMap("public_id", avatarNameOnCloud, "overwrite", true, "upload_preset", "cougarstrore");
+		try {
+			Object res = cloudinary.uploader().upload(user.getAvatar(), params);
+			String url = ((Map<String, String>) res).get("url").replace("http://res.cloudinary.com/dmjh7imwd/image/upload/", "");
+			oldUser.setAvatar(url);
+			String oladPass = oldUser.getPassword();
+			user.setPassword(oladPass);
+			return ResponseEntity.ok(userService.update(oldUser));
+		} catch (IOException exception) {
+			return ResponseEntity.badRequest().body(new MessageResponse("Có lỗi khi xử lý hình ảnh, hãy thử upload lại sau."));
+		}
+	}
+
+	@PutMapping("/update-admin")
+	public ResponseEntity<?> update(@RequestBody User user) {
+		User oldUser = userService.findById(user.getId());
+		if (!user.getEmail().equals(oldUser.getEmail())) {
+			if (userLoginDAO.existsByEmail(user.getEmail())) {
+				return ResponseEntity.badRequest().body(new MessageResponse("Email already exists!"));
+			}
+		}
+		if (!user.getPhone().equals(oldUser.getPhone())) {
+			if (userLoginDAO.existsByPhone(user.getPhone())) {
+				return ResponseEntity.badRequest().body(new MessageResponse("Phone already exists!"));
+			}
+		}
 		String oladPass = oldUser.getPassword();
 		user.setPassword(oladPass);
 		return ResponseEntity.ok(userService.update(user));
-	}
-
-	@DeleteMapping("/{id}")
-	public void delete(@PathVariable("id") Integer id) {
-		userService.deleteById(id);
-	}
-
-	@GetMapping("/is-admin")
-	public List<User> isAdmin() {
-		return userService.isAdmin();
-	}
-
-	@GetMapping("/is-user")
-	public List<User> isUser() {
-		return userService.isUser();
 	}
 
 	@PutMapping("/update/{id}")
